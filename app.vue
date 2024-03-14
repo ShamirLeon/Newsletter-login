b9f0bc576ce28944ea1076f5a7665f4a591631d7
Shamir Leon <leonshamir1@gmail.com>
2024-03-14 14:57:18 -0500

    add modal

--- app.vue
+++ app.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="flex justify-center items-center h-[100vh] bg-DarkSlateGrey">
-    <div class="flex bg-white p-8 rounded-[24px] gap-8 ">
-      <div class="grid font-Roboto place-content-center  w-[500px]">
-        <div class="w-[90%]">
-          <h1 class="text-DarkSlateGrey text-6xl font-bold mb-8">Stay updated!</h1>
-          <p class="mb-8">Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul class="mb-4">
-            <li v-for="item in items" class="mb-3">
-              <img src="./assets/images/icon-list.svg" class="inline-block mr-3">
+  <div :class="classes.container">
+    <div :class="classes.card.container">
+      <div :class="classes.card.grid">
+        <div :class="classes.card.leftColumn">
+          <h1 :class="classes.card.title">Stay updated!</h1>
+          <p :class="classes.card.paragraph">Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul :class="classes.card.list">
+            <li v-for="item in items" :class="classes.card.listItem">
+              <img src="./assets/images/icon-list.svg" :class="classes.card.listIcon">
               {{ item }}
             </li>
           </ul>
-          <form action="" class="flex flex-col gap-2">
-            <label for="email" class="font-bold text-sm">Email address</label>
-            <input type="email" name="email" id="" placeholder="email@company.com" class="py-4 px-6 rounded-lg border mb-4 focus:outline-none">
-            <button type="submit" class="bg-DarkSlateGrey text-white py-4 rounded-lg border border-DarkSlateGrey hover:bg-gradient-to-r hover:from-Pink  hover:to-Orange hover:border-transparent transition ease-in-out">Subscribe to monthly newsletter</button>
+          <form :class="classes.card.form">
+            <label for="email" :class="classes.card.label">Email address</label>
+            <input v-model="email" type="email" name="email" id="" placeholder="email@company.com" :class="classes.card.input">
+            <button @click.prevent="handleModal" :class="classes.card.submit">Subscribe to monthly newsletter</button>
           </form>
         </div>
       </div>
@@ -22,11 +22,45 @@
         <img src="./assets/images/illustration-sign-up-desktop.svg" alt="Sing up" />
       </div>
     </div>
+
+    <Modal v-if="modalShow" :email="email" @dissmiss="handleModal" />
   </div>
 </template>
 
 <script setup>
+import Modal from './components/modal/Modal.vue'
+import { ref } from 'vue'
+
+/* Refs variables */
+let email = ref('')
+const modalShow = ref(false)
 
 const items = ['Product discovery and building what matters', 'Measuring to ensure updates are a success', 'And much more!']
+
+/* Classes styles */
+const classes = {
+  container: 'flex justify-center items-center h-[100vh] bg-DarkSlateGrey',
+  card: {
+    container: 'flex bg-white p-8 rounded-[24px] gap-8',
+    grid: 'grid font-Roboto place-content-center w-[500px]',
+    leftColumn: 'w-[90%]',
+    title: 'text-DarkSlateGrey text-6xl font-bold mb-8',
+    paragraph: 'mb-8',
+    list: 'mb-8',
+    listItem: 'mb-3',
+    listIcon: 'inline-block mr-3',
+    image: 'inline-block mr-3',
+    form: 'flex flex-col gap-2',
+    label: 'font-bold text-sm',
+    input: 'py-4 px-6 rounded-lg border mb-4 focus:outline-none',
+    submit: 'transition-all bg-DarkSlateGrey text-white py-4 rounded-lg border border-DarkSlateGrey hover:bg-gradient-to-r hover:from-Pink hover:to-Orange hover:border-transparent'
+  },
+}
+
+/* Methods */
+const handleModal = () => {
+  modalShow.value = !modalShow.value
+}
+
 
 </script>
---
+++ components/modal/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="classes.container">
+        <div :class="classes.card.container">
+            <!-- Modal content goes here -->
+            <img src="../../assets/images/icon-success.svg" :class="classes.card.icon">
+            <h2 :class="classes.card.title">Thanks for subscribing!</h2>
+            <p :class="classes.card.paragraph">
+                A confirmation email has been sent to <span :class="classes.card.span">{{ email }}</span>. Please open it and click the button inside to confirm your subscription.
+            </p>
+            <button @click="dissmiss" :class="classes.card.button">Dissmiss mesage</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { defineProps, defineEmits } from 'vue'
+
+/* Define props */
+const props = defineProps({
+    email: String,
+})
+
+/* Define emits */
+const emits = defineEmits(['dissmiss'])
+
+/* Classes styles */
+const classes = {
+    container: 'fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center backdrop-blur-sm',
+    card: {
+        container: 'bg-white p-8 rounded-3xl max-w-96',
+        icon: 'w-[20%] mb-6',
+        title: 'text-DarkSlateGrey text-4xl font-bold mb-8',
+        span: 'font-bold',
+        paragraph: 'mb-6',
+        button: 'transition-all w-full text-sm font-bold tracking-wider text-center bg-DarkSlateGrey text-white py-3 rounded-lg border border-DarkSlateGrey hover:bg-gradient-to-r hover:from-Pink hover:to-Orange hover:border-transparent'
+    }
+}
+
+/* Methods */
+const dissmiss = () => {
+    emits('dissmiss', false)
+}
+</script>
